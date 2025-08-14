68ac4c9cc066847df3fd9b18d776f47e1bd11b81
wangz1yu <49710008+wangz1yu@users.noreply.github.com>
2025-08-14 10:04:24 +0000

    Fix menu model fields to match database schema

--- server/models/menuList.js
+++ server/models/menuList.js
@@ -1,6 +1,6 @@
 const { sequelize, DataTypes } = require("../config/connect") // 引入相关依赖以及数据库
 const MenuList = sequelize.define(
-  "aw_menu_list", // 数据库中生成的表名(最终会自动在末尾添加s,因此最终数据库中生自动成表名为accounts
+  "aw_menu_list", // 数据库中生成的表名
   { // 表字段信息配置
     id: {
       type: DataTypes.INTEGER, // 字段类型
@@ -11,31 +11,41 @@ const MenuList = sequelize.define(
     parent_id:{
       type: DataTypes.INTEGER, // 字段类型
       allowNull: true, // 是否允许为空
+      defaultValue: 0,
       comment:'父级ID'
     },
-    name: {
+    menu_name: {
       type: DataTypes.STRING, // 字段类型
       allowNull: false, // 是否允许为空
-      comment:'名称'
+      comment:'菜单名称'
     },
-    title: {
+    menu_url: {
       type: DataTypes.STRING, // 字段类型
-      allowNull: false, // 是否允许为空
-      comment:'标题名称'
+      allowNull: true, // 是否允许为空
+      comment:'菜单URL'
     },
-    icon: {
+    menu_icon: {
       type: DataTypes.STRING, // 字段类型
       allowNull: true, // 是否允许为空
       comment:'菜单图标'
     },
-    path: {
-      type: DataTypes.STRING, // 字段类型
+    sort_order: {
+      type: DataTypes.INTEGER, // 字段类型
       allowNull: false, // 是否允许为空
-      comment:'菜单路径'
+      defaultValue: 0,
+      comment:'排序'
+    },
+    status: {
+      type: DataTypes.INTEGER, // 字段类型
+      allowNull: false, // 是否允许为空
+      defaultValue: 1,
+      comment:'状态'
     }
   },
   {
     timestamps: true, // 是否自动在数据表中添加数据操作时间
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
   }
 )
 
